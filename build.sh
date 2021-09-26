909ce3b20ed24a09f4299c76f305ee188a54ad79
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2021-09-26 19:29:55 +0200

    Moved gnome shell files to src directory

--- build.sh
+++ build.sh
@@ -1,1 +1,15 @@
-sass --no-source-map ./sass/gnome-shell.scss ./gnome-shell.css
+#!/bin/bash
+
+# ----------- CLEANING  -------------
+rm -rf ./build/*
+cp ./src/index.theme ./build/index.theme
+
+# ----------- GNOME SHELL -----------
+mkdir -p ./build/gnome-shell/
+sass --no-source-map./src/gnome-shell/sass/gnome-shell.scss ./build/gnome-shell/gnome-shell.css
+cp -r ./src/gnome-shell/assets ./build/gnome-shell/
+cp -r ./src/gnome-shell/icons ./build/gnome-shell/
+cp ./src/gnome-shell/gnome-shell-theme.gresource ./build/gnome-shell/gnome-shell-theme.gresource
+cp ./src/gnome-shell/gnome-shell-theme.gresource.xml ./build/gnome-shell/gnome-shell-theme.gresource.xml
+cp ./src/gnome-shell/*.svg ./build/gnome-shell/
+cp ./src/gnome-shell/*.css ./build/gnome-shell/
